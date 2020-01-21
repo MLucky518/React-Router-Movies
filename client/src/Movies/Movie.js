5776cc108c168cd93fe28ab92c09dc5b4f7e3605
MLucky518 <lucky32815@gmail.com>
2020-01-20 22:57:24 -0500

    mvp soe stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  
   useEffect(() => {
-    const id = 1;
+   const id = props.match.params.id;
+   // const id = props.movie.find(movie1 => `${movie1.id}` === props.match.params.id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -20,6 +22,7 @@ const Movie = (props) => {
 
   },[]);
   
+  
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
@@ -32,25 +35,7 @@ const Movie = (props) => {
 
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+    <MovieCard title = {movie.title} director = {movie.director} metascore = {movie.metascore} star = {movie.stars}/>
   );
 }
 
